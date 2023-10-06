e40ecf4182fd7e9a6aa9f7d30317510835364ed3
HafthorArni <111596842+HafthorArni@users.noreply.github.com>
2023-10-06 16:33:46 +0000

    Add files via upload

--- main_part3.cpp
+++ main_part3.cpp
@@ -11,8 +11,11 @@ Timer_msec timer;
 Digital_out motorIN2(0); //         pin D8
 Analog_out motorIN1(1);  // PWM     pin D9
 Encoder encoder(3, 4);   // encoder pin D11 D12
-float kp = 1;
+float kp = 0.015;
 P_controller controller(kp);
+
+
+unsigned long lastPrintTime = 0;
 
 int analogPin = A3; 
 double ref = 0;
@@ -20,14 +23,9 @@ double actual = 0;
 double pwmValue = 0;
 double u = 0;
 
-float mapFloat(float value, float fromLow, float fromHigh, float toLow, float toHigh) {
-    return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
-}
-
-
 int main(){  
     init();// Initialize Arduino framework
-    Serial.begin(115200);  
+    Serial.begin(9600);  
     timer.init(0.1); // ms
     sei();  // enable interrupts
 
@@ -41,23 +39,23 @@ int main(){
         ref = (analogRead(analogPin)/1023.0)*120;
         actual = encoder.speed();
         u = controller.update(ref, actual);
-        //pwmValue = constrain(pwmValue, 0.0, 0.999); // Ensure pwmValue is within [0, 1]
-        pwmValue = mapFloat(u, -kp*120.0, kp*120.0, 0.0, 0.999);
-        //pwmValue = pwmValue/(120);
+        pwmValue = constrain(u, 0.0, 0.999); // Ensure pwmValue is within [0, 1]
 
         motorIN1.set(pwmValue);
-        Serial.print("speed: (");
-        Serial.print("Ref: ");
-        Serial.print(ref);
-        Serial.print(" - Act: ");
-        Serial.print(actual);
-        Serial.print(") [RPM], ");
-        Serial.print(" duty cycle: ");
-        Serial.print(pwmValue);
-        Serial.print(" u: ");
-        Serial.println(u);
-        
-        _delay_ms(100);
+        if (millis() - lastPrintTime >= 1000) {  
+            Serial.print("speed: (");
+            Serial.print("Ref: ");
+            Serial.print(ref);
+            Serial.print(" - Act: ");
+            Serial.print(actual);
+            Serial.print(") [RPM], ");
+            Serial.print(" duty cycle: ");
+            Serial.print(pwmValue);
+            Serial.print(" u: ");
+            Serial.println(u);
+            lastPrintTime = millis();  
+        }
+        encoder.update();
     }
     return 0;
 }
