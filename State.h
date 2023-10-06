1a34e9ccc02db5eadd7b7ce69268859dfc5d14e4
ragnheidurgna <ragnheidurg23@ru.is>
2023-10-06 17:13:23 +0000

    Klasarnir mættir

---
+++ State.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <Context.h>
+
+class State
+{
+  /**
+   * @var Context
+   */
+
+protected:
+  Context *context_;
+
+public:
+
+  virtual ~State();
+
+  void set_context(Context *context);
+
+  virtual void on_do() = 0;
+  virtual void on_entry() = 0;
+  virtual void on_exit() = 0;
+  virtual void on_command_go() = 0;
+  virtual void on_command_stop() = 0;
+  virtual void on_timeout() = 0;
+
+  // ...
+
+};
---
+++ States.h
@@ -0,0 +1,202 @@
+#include <Arduino.h>
+#include <State.h>
+bool pre;
+bool op;
+bool flt;
+char c;
+
+class Initialization : public State {
+    public:
+
+    void on_do() override
+    {
+
+    }
+    void on_entry() override
+    {
+        Serial.println("Turning off LED");
+        Serial.println("Initializing c");
+        Serial.println("Setting pre = false");
+        pre = false;
+        Serial.println("Setting op = true");
+        op = true;
+        Serial.println("Setting FLT = false");
+        flt = false;
+        
+    }
+    void on_exit() override
+    {
+
+    }
+    void on_command_go() override
+    {
+    }
+
+    void on_command_stop() override
+    {
+    }
+
+    void on_timeout() override;
+    // void on_event1();
+    // void on_event2();
+
+};
+
+class PreOperational : public State 
+{
+    public:
+    void on_do() override
+    {
+        Serial.println("Doing fault check");
+
+        Serial.println("Setting c = '0'");
+        c = '0';
+        Serial.println("Reading into c from keyboard");
+        c = Serial.read();
+    }
+    void on_entry() override
+    {
+        Serial.println("Blinking LED at 1 Hz");
+        Serial.println("Setting pre = true");
+        pre = true;
+    }
+    void on_exit() override
+    {
+
+    }
+    void on_command_go() override
+    {
+    }
+
+    void on_command_stop() override
+    {
+    }
+
+    void on_timeout() override;
+    // void on_event1();
+    // void on_event2();
+};
+
+class Operational : public State 
+{
+    public:
+    void on_do() override
+    {
+        Serial.println("Doing fault check");
+
+        Serial.println("Setting c = '0'");
+        c = '0';
+        Serial.println("Reading into c from keyboard");
+        c = Serial.read();
+    }
+    void on_entry() override
+    {
+        Serial.println("Turning on LED");
+        Serial.println("Setting pre = false");
+        pre = false;
+    }
+    void on_exit() override
+    {
+
+    }
+    void on_command_go() override
+    {
+    }
+
+    void on_command_stop() override
+    {
+    }
+
+    void on_timeout() override;
+    // void on_event1();
+    // void on_event2();
+
+};
+
+class Stopped : public State {
+    public:
+    void on_do() override
+    {
+        Serial.println("Setting c = '0'");
+        c = '0';
+        Serial.println("Reading into c from keyboard");
+        c = Serial.read();
+    }
+    void on_entry() override
+    {
+        Serial.println("Blinking LED at 1 Hz");
+    }
+    void on_exit() override
+    {
+        Serial.println("Setting flt = false");
+        flt = false;
+    }
+    void on_command_go() override
+    {
+    }
+
+    void on_command_stop() override
+    {
+    }
+
+    void on_timeout() override;
+    // void on_event1();
+    // void on_event2();
+};
+
+// void Initialization::on_command_go()
+// {
+//     Serial.println("IN:op = true - We are operational");
+//     this->context_->transition_to(new PreOperational);
+// }
+
+void PreOperational::on_command_go()
+{
+    Serial.println("PRE:Leaving pre");
+    if (c == 'o'){
+        Serial.println("Going to OP");
+        this->context_->transition_to(new Operational);
+    } 
+    if (c == 'r'){
+        Serial.println("Going to IN");
+        this->context_->transition_to(new Initialization);
+    }
+}
+
+void PreOperational::on_command_stop()
+{
+    Serial.println("PRE:Stop command detected - Stopping");
+    this->context_->transition_to(new Stopped);
+}
+
+void Operational::on_command_go()
+{
+    Serial.println("OP:Leaving OP");
+    if (c == 'p'){
+        Serial.println("Going to PRE");
+        this->context_->transition_to(new PreOperational);
+    } 
+    if (c == 'r'){
+        Serial.println("Going to IN");
+        this->context_->transition_to(new Initialization);
+    }
+}
+
+void Operational::on_command_stop()
+{
+    Serial.println("OP:Stop command detected - Stopping");
+    this->context_->transition_to(new Stopped);
+}
+
+void Stopped::on_command_go()
+{
+    Serial.println("STO:Continue command detected - Continue");
+    if (pre){
+        this->context_->transition_to(new PreOperational);
+    } else{
+        this->context_->transition_to(new Operational);
+    }
+}
+
+
+
